91529f605511c555f290bdd06831c0841cbda1af
SvetlanaB <kapk2004@inbox.ru>
2022-10-07 17:46:28 +0300

    Task3

--- Seminar3.py/Task3.py
+++ Seminar3.py/Task3.py
@@ -1,13 +1,38 @@
-# Задайте список из вещественных чисел. Напишите программу, которая найдёт разницу между максимальным и минимальным значением дробной части элементов. Если число целое, то его дробная часть не считается за 0 и оно (число) в сравнении не участвует
+# Задайте список из вещественных чисел. Напишите программу, которая найдёт разницу между максимальным 
+# и минимальным значением дробной части элементов. Если число целое, то его дробная часть не считается за 0 и оно (число) в сравнении не участвует
 
 # Пример:
 # [1.1, 1.2, 3.1, 5, 10.01] => 0.19
 
-list=[1.1, 1.2, 3.1, 5, 10.01]
+from audioop import minmax
+
+list=[62.12, 5.89, 15.07, 6.21, 2.35, 4.08]
 print(list)
 
+newlist=[]
+
 for i in range(len(list)):
-    el=round(list[i]%10)
-    print(el)
-    
-   
+    el=round(list[i]%1,2)
+    if el!=0:
+        newlist.append(el)    
+print(newlist)
+
+# def minmax(newlist):
+#     newlist[0]=n_min
+#     newlist[0]=n_max 
+#     for i in range(len(newlist)):
+#         if  i<n_min:
+#             n_min=i
+#         else:
+#             if i>n_max:
+#                 i=n_max
+#             else:
+#               print(n_max,n_min)
+#     return(n_max,n_min)      
+
+# minmax(newlist)
+a=min(newlist)
+b=max(newlist)
+
+result=b-a
+print("=>",round((result),2))
