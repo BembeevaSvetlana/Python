e2e9fa7e464000690bc884b9afeedba9c1b99435
BembeevaSvetlana <110349192+BembeevaSvetlana@users.noreply.github.com>
2022-10-18 22:39:18 +0300

    Update Task3.py
    delete comments

--- Seminar5.py/Task3.py
+++ Seminar5.py/Task3.py
@@ -1,11 +1,9 @@
 # Создайте программу для игры в 'Крестики-нолики'.
 
-# Инициализация карты
 maps = [1,2,3,
         4,5,6,
         7,8,9]
  
-# Инициализация победных линий
 victories = [[0,1,2],
              [3,4,5],
              [6,7,8],
@@ -15,7 +13,6 @@ victories = [[0,1,2],
              [0,4,8],
              [2,4,6]]
  
-# Вывод карты на экран
 def print_maps():
     print(maps[0], end = " ")
     print(maps[1], end = " ")
@@ -29,16 +26,13 @@ def print_maps():
     print(maps[7], end = " ")
     print(maps[8])    
  
-# Сделать ход в ячейку
 def step_maps(step,symbol):
     ind = maps.index(step)
     maps[ind] = symbol
  
-# Получить текущий результат игры
 def get_result():
     win = ""
- 
-    for i in victories:
+     for i in victories:
         if maps[i[0]] == "X" and maps[i[1]] == "X" and maps[i[2]] == "X":
             win = "X"
         if maps[i[0]] == "O" and maps[i[1]] == "O" and maps[i[2]] == "O":
@@ -46,32 +40,27 @@ def get_result():
              
     return win
  
-# Основная программа
 game_over = False
 player1 = True
  
 while game_over == False:
- 
-    # 1. Показываем карту
-    print_maps()
- 
-    # 2. Спросим у играющего куда делать ход
-    if player1 == True:
+      print_maps()
+      
+        if player1 == True:
          symbol = "X"
          step = int(input("Player 1, ваш ход: "))
     else:
          symbol = "O"
          step = int(input("Player 2, ваш ход: "))
  
-    step_maps(step,symbol) # делаем ход в указанную ячейку
-    win = get_result() # определим победителя
+    step_maps(step,symbol) 
+    win = get_result() 
     if win != "":
         game_over = True
     else:
         game_over = False
  
     player1 = not(player1)        
- 
-# Игра окончена. Покажем карту. Объявим победителя.        
+  
 print_maps()
 print("Победил", win)
