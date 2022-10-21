df1ba9e5cb5aa8e04ed29e2504f788327e4bf12b
SvetlanaB <kapk2004@inbox.ru>
2022-10-21 03:16:44 +0300

    Task1-list comprehansion

---
+++ Seminar6.py/Task1.py
@@ -0,0 +1,39 @@
+import random
+# Реализуйте алгоритм перемешивания списка, без использования встроеных методов (особенно SHUFFLE, без него) можно (нужно) использовать библиотеку Random
+# mylist=[]
+# for _ in range(10):
+#     mylist.append(random.randint(0,50))
+# print(mylist)
+# # mylist=[1,2,3,4,5]
+# newlist=[]
+# newlist=mylist[:]
+
+
+# for j in range(0,len(newlist)):
+#     #print(newlist)
+#     ind=random.randint(0,len(newlist)-1)
+    
+#     temp=newlist[j]
+#     newlist[j]=newlist[ind]
+#     newlist[ind]=temp
+   
+# print(newlist)
+
+# Измененный код
+mylist=[random.randint(0,50) for k in range(10)]
+print(mylist)
+
+def mix_el(mylist):
+    newlist=mylist[:]
+
+    for j in range(0,len(newlist)):
+    #print(newlist)
+        ind=random.randint(0,len(newlist)-1)
+    
+        temp=newlist[j]
+        newlist[j]=newlist[ind]
+        newlist[ind]=temp 
+
+    return newlist 
+mix_list=mix_el(mylist)
+print(mix_list)
